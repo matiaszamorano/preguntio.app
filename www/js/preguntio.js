2393c35ecfb6bd90519d751bf8c790c6821bce4c
Uva <Uva@Uva-PC>
2016-12-01 01:04:00 -0300

    Cambios y consulta al server

--- www/js/preguntio.js
+++ www/js/preguntio.js
@@ -2,44 +2,14 @@ var template = $$('#pregunta-template').html();
 
 var compiledTemplate = Template7.compile(template);
 
-var context = {
-    "preguntas": [{
-            "contenido": "¿Qué expectativas tenés para el año que viene?",
-            "_links": {
-                "self": {
-                    "href": "https://preguntio.herokuapp.com/preguntas/1"
-                },
-                "pregunta": {
-                    "href": "https://preguntio.herokuapp.com/preguntas/1"
-                }
-            }
-        }, {
-            "contenido": "¿En qué cosa nueva querés invertir tu tiempo?",
-            "_links": {
-                "self": {
-                    "href": "https://preguntio.herokuapp.com/preguntas/2"
-                },
-                "pregunta": {
-                    "href": "https://preguntio.herokuapp.com/preguntas/2"
-                }
-            }
-        }, {
-            "contenido": "¿Qué le contas a otros de tu lugar de trabajo?",
-            "_links": {
-                "self": {
-                    "href": "https://preguntio.herokuapp.com/preguntas/3"
-                },
-                "pregunta": {
-                    "href": "https://preguntio.herokuapp.com/preguntas/3"
-                }
-            }
-        }]
-};
-var html = compiledTemplate(context);
+$$.get('https://preguntio.herokuapp.com/preguntas', null, function (data) {
+    var html = compiledTemplate(JSON.parse(data)._embedded);
 
-$$('#preguntas-slide').html(html);
+    $$('#preguntas-slide').html(html);
 
-var mySwiper = myApp.swiper('.swiper-container', {
-    speed: 400,
-    spaceBetween: 100
-});   
+    var mySwiper = myApp.swiper('.swiper-container', {
+        speed: 400,
+        spaceBetween: 100
+    });
+
+});
