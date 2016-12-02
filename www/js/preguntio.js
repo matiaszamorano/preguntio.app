9f0c112cfadf9cca08c5acde0b3d58a90905b4a6
uva <uva@CT15PC014>
2016-12-02 13:58:41 -0300

    Arreglos en la botonera

--- www/js/preguntio.js
+++ www/js/preguntio.js
@@ -2,14 +2,14 @@ var template = $$('#pregunta-template').html();
 
 var compiledTemplate = Template7.compile(template);
 
-//$$.get('https://preguntio.herokuapp.com/preguntas', null, function (data) {
-//    var html = compiledTemplate(JSON.parse(data)._embedded);
-//
-//    $$('#preguntas-slide').html(html);
-//
-//    var mySwiper = myApp.swiper('.swiper-container', {
-//        speed: 400,
-//        spaceBetween: 100
-//    });
-//
-//});
+$$.get('https://preguntio.herokuapp.com/preguntas', null, function (data) {
+    var html = compiledTemplate(JSON.parse(data)._embedded);
+
+    $$('#preguntas-slide').html(html);
+
+    var mySwiper = myApp.swiper('.swiper-container', {
+        speed: 400,
+        spaceBetween: 100
+    });
+
+});
