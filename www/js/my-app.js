7ad9815eabc3fdf5e519e5812893bd26e7d8c0ec
uva <uva@CT15PC014>
2016-12-06 13:14:26 -0300

    Welcome home configurada

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -1,10 +1,53 @@
 var myApp = new Framework7({
     material: true
 });
-var mainView = myApp.addView('.view-main');
 
 var $$ = Dom7;
 
-$$('a').on('click', function (e) { //Close panel when you open a new page
-    myApp.closePanel();
-});
+var mainView = myApp.addView('.view-main');
+
+myApp.init();
+
+//''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+// 
+// Tutorial inicial
+// 
+//''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+initTutorial();
+
+function initTutorial() {
+    var welcomescreen_slides = [
+        {
+            id: 'slide0',
+            picture: '<div class="pnt-tutorial-icon">☆</div>',
+            text: 'Bienvenido a la aplicación de avisos de Fravega.'
+        },
+        {
+            id: 'slide1',
+            picture: '<div class="pnt-tutorial-icon">✲</div>',
+            text: 'Selecciná los productos y el porcentaje de descuento de tu interes.'
+        },
+        {
+            id: 'slide2',
+            picture: '<div class="pnt-tutorial-icon">♫</div>',
+            text: 'Nosotros nos encargamos de avisarte cuando salgan promociones de tu interés.<br><br><a class="pnt-tutorial-close-btn pnt-js-tutorial-close-btn" href="#">Empezar</a>'
+        }
+    ];
+
+    var options = {
+        bgcolor: '#009688',
+        fontcolor: '#FFF',
+        closeButtonText: 'Saltar'
+    };
+
+    var welcomescreen = myApp.welcomescreen(welcomescreen_slides, options);
+
+    $$(".pnt-js-tutorial-close-btn").click(function () {
+        welcomescreen.close();
+        initApp();
+    });
+}
+//
+//$$('a').on('click', function (e) { //Close panel when you open a new page
+//    myApp.closePanel();
+//});
--- www/js/preguntio.js
+++ www/js/preguntio.js
@@ -1,72 +1,76 @@
-recargarPreguntas();
+function initApp() {
+    recargarPreguntas();
 
-var mySwiper;
+    var mySwiper;
 
-function recargarPreguntas() {
-    var template = $$('#pregunta-template').html();
+    function recargarPreguntas() {
+        var template = $$('#pregunta-template').html();
 
-    var compiledTemplate = Template7.compile(template);
+        var compiledTemplate = Template7.compile(template);
 
-    $$.get('https://preguntio.herokuapp.com/preguntas', null, function (data) {
-        var html = compiledTemplate(JSON.parse(data)._embedded);
+        $$.get('https://preguntio.herokuapp.com/preguntas', null, function (data) {
+            var html = compiledTemplate(JSON.parse(data)._embedded);
 
-        $$('#preguntas-slide').html(html);
+            $$('#preguntas-slide').html(html);
 
-        mySwiper = myApp.swiper('.swiper-container', {
-            speed: 400,
-            spaceBetween: 100
+            mySwiper = myApp.swiper('.swiper-container-preguntas', {
+                speed: 400,
+                spaceBetween: 100
+            });
+
+            $$('.borrar-pregunta').on('click', borrarPregunta);
         });
+    }
 
-        $$('.borrar-pregunta').on('click', borrarPregunta);
+    $$(document).on('pageInit', function () {
+        $$('#enviar-pregunta').on('click', crearPregunta);
     });
+
+    function crearPregunta() {
+        queryForm = myApp.formToJSON('#nueva-pregunta-form');
+        var parameters = {
+            url: 'https://preguntio.herokuapp.com/preguntas',
+            method: "POST",
+            cache: false,
+            contentType: "application/json",
+            processData: true,
+            success: function (data) {
+                myApp.mainView.back();
+                myApp.addNotification({
+                    message: 'Pregunta Enviada',
+                    closeOnClick: true
+                });
+                recargarPreguntas();
+            },
+            dataType: 'json',
+            data: JSON.stringify(queryForm)
+        };
+
+        $$.ajax(parameters);
+        return false;
+    }
+
+    function borrarPregunta() {
+        var url = $$(this).attr('data-url');
+        var parameters = {
+            url: url,
+            method: "DELETE",
+            processData: true,
+            complete: function (data) {
+                mySwiper.removeSlide(mySwiper.activeIndex);
+                myApp.addNotification({
+                    message: 'Pregunta Eliminada',
+                    closeOnClick: true,
+                });
+            },
+            dataType: 'json'
+        };
+        $$.ajax(parameters);
+
+        return false;
+    }
+
 }
 
-$$(document).on('pageInit', function () {
-    $$('#enviar-pregunta').on('click', crearPregunta);
-});
-
-function crearPregunta() {
-    queryForm = myApp.formToJSON('#nueva-pregunta-form');
-    var parameters = {
-        url: 'https://preguntio.herokuapp.com/preguntas',
-        method: "POST",
-        cache: false,
-        contentType: "application/json",
-        processData: true,
-        success: function (data) {
-            myApp.mainView.back();
-            myApp.addNotification({
-                message: 'Pregunta Enviada',
-                closeOnClick: true
-            });
-            recargarPreguntas();
-        },
-        dataType: 'json',
-        data: JSON.stringify(queryForm)
-    };
-
-    $$.ajax(parameters);
-    return false;
-}
-
-function borrarPregunta() {
-    var url = $$(this).attr('data-url');
-    var parameters = {
-        url: url,
-        method: "DELETE",
-        processData: true,
-        complete: function (data) {
-            mySwiper.removeSlide(mySwiper.activeIndex);
-            myApp.addNotification({
-                message: 'Pregunta Eliminada',
-                closeOnClick: true,
-            });
-        },
-        dataType: 'json'
-    };
-    $$.ajax(parameters);
-
-    return false;
-}
 
 
