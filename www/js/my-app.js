576bea7fde6ba58a6524141e3c328b740c1bdf72
uva <uva@CT15PC014>
2016-12-06 14:49:11 -0300

    Iconos en el tutorial

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -19,18 +19,18 @@ function initTutorial() {
     var welcomescreen_slides = [
         {
             id: 'slide0',
-            picture: '<div class="pnt-tutorial-icon">☆</div>',
-            text: 'Bienvenido a la aplicación de avisos de Fravega.'
+            picture: '<div class="pnt-tutorial-icon"><i class="material-icons">live_help</i></div>',
+            text: 'Bienvenido a Pregunt.io, la aplicación para descubrir preguntas.'
         },
         {
             id: 'slide1',
-            picture: '<div class="pnt-tutorial-icon">✲</div>',
-            text: 'Selecciná los productos y el porcentaje de descuento de tu interes.'
+            picture: '<div class="pnt-tutorial-icon"><i class="material-icons">fingerprint</i></div>',
+            text: 'Las preguntas tienen el poder de generar ideas reveladoras.'
         },
         {
             id: 'slide2',
-            picture: '<div class="pnt-tutorial-icon">♫</div>',
-            text: 'Nosotros nos encargamos de avisarte cuando salgan promociones de tu interés.<br><br><a class="pnt-tutorial-close-btn pnt-js-tutorial-close-btn" href="#">Empezar</a>'
+            picture: '<div class="pnt-tutorial-icon"><i class="material-icons">star_border</i></div>',
+            text: 'Guardá tus preguntas favoritas.<br><br><a class="pnt-tutorial-close-btn pnt-js-tutorial-close-btn" href="#">Empezar</a>'
         }
     ];
 
