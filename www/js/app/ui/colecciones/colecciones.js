3d3f4e048909032fc15f0a07b4e7de854bc964ba
uva <uva@CT15PC014>
2016-12-23 13:42:55 -0300

    Traigo las colecciones del servicio

--- www/js/app/ui/colecciones/colecciones.js
+++ www/js/app/ui/colecciones/colecciones.js
@@ -1,7 +1,16 @@
 preguntio.ui.colecciones = (function () {
 
     function init() {
-        console.log("colecciones");
+        var template = $$('#coleccion-template').html();
+
+        var compiledTemplate = Template7.compile(template);
+
+        preguntio.service.colecciones.get(function (data) {
+
+            var colecciones = JSON.parse(data)._embedded;
+            var html = compiledTemplate(colecciones);
+            $$('#colecciones').html(html);
+        });
     }
 
     return {
