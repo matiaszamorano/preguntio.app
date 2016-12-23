dd0ef91df0b08836c778c1b5f09b7e995acec036
uva <uva@CT15PC014>
2016-12-23 17:09:26 -0300

    Trayendo la data del server

--- www/js/app/ui/categorias/categorias.js
+++ www/js/app/ui/categorias/categorias.js
@@ -1,7 +1,20 @@
 preguntio.ui.categorias = (function () {
 
-    function init() {
-        console.log("categorias");
+    function init(page) {
+        $$.get(page.context.href, null, function (data) {
+            var coleccion = JSON.parse(data);
+            $$(".titulo-categoria").text(coleccion.titulo);
+            $$(".descripcion-categoria").text(coleccion.descripcion);
+            $$.get(coleccion._links.categorias.href, null, function (data) {
+                var template = $$('#categoria-template').html();
+                var compiledTemplate = Template7.compile(template);
+                var categorias = JSON.parse(data)._embedded;
+                console.log(categorias);
+                var html = compiledTemplate(categorias);
+                console.log(html);
+                $$('#listado-categorias').html(html);
+            });
+        });
     }
 
     return {
--- www/js/app/ui/colecciones/colecciones.js
+++ www/js/app/ui/colecciones/colecciones.js
@@ -5,12 +5,12 @@ preguntio.ui.colecciones = (function () {
 
         var compiledTemplate = Template7.compile(template);
 
-        preguntio.service.colecciones.get(function (data) {
-
+        $$.get('https://preguntio.herokuapp.com/api/colecciones', null, function (data) {
             var colecciones = JSON.parse(data)._embedded;
             var html = compiledTemplate(colecciones);
             $$('#colecciones').html(html);
         });
+
     }
 
     return {
