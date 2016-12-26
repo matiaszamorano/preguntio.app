5db5b6adf0f4982824ff00ccc7c5bdd2be5d98e6
Uva <Uva@Uva-PC>
2016-12-26 19:56:17 -0300

    Voy a la cache

--- www/js/app/service/service.js
+++ www/js/app/service/service.js
@@ -1,18 +1,30 @@
 preguntio.service = (function () {
 
     var storage = window.localStorage;
+    var tiempoCache = 604800000; //10 días
 
     function get(uri, callback) {
         if (localStorage.getItem(uri) === null) {
-            $$.get(uri, null, function (data) {
-                console.log("server");
-                storage.setItem(uri, data);
-                callback(storage.getItem(uri));
-            });
+            getFromServer(uri, callback);
         } else {
-            console.log("cache");
-            callback(storage.getItem(uri));
+            var object = JSON.parse(storage.getItem(uri));
+            var dateString = object.timestamp;
+            var now = new Date().getTime().toString();
+            var diferencia = now - dateString;
+            if (diferencia >= tiempoCache) {
+                getFromServer(uri, callback);
+            } else {
+                callback(object.value);
+            }
         }
+    }
+
+    function getFromServer(uri, callback) {
+        $$.get(uri, null, function (data) {
+            var object = {value: data, timestamp: new Date().getTime()};
+            storage.setItem(uri, JSON.stringify(object));
+            callback(JSON.parse(storage.getItem(uri)).value);
+        });
     }
 
     return {
