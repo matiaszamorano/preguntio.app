2f1ad2c250718fc8e01b8c50ebab6dde6142847c
Uva <Uva@Uva-PC>
2016-12-26 23:39:28 -0300

    Saco lo del tiempo del storage

--- www/js/app/service/service.js
+++ www/js/app/service/service.js
@@ -1,31 +1,20 @@
 preguntio.service = (function () {
 
     var storage = window.localStorage;
-    var tiempoCache = 604800000; //10 días
-    //var tiempoCache = 100; //no cache
 
     function get(uri, callback) {
         if (localStorage.getItem(uri) === null) {
             getFromServer(uri, callback);
         } else {
-            var object = JSON.parse(storage.getItem(uri));
-            var dateString = object.timestamp;
-            var now = new Date().getTime().toString();
-            var diferencia = now - dateString;
-            if (diferencia >= tiempoCache) {
-                getFromServer(uri, callback);
-            } else {
-                callback(object.value);
-                getFromServer(uri,function(){});
-            }
+            callback(storage.getItem(uri));
+            getFromServer(uri, function () {});
         }
     }
-
+    
     function getFromServer(uri, callback) {
         $$.get(uri, null, function (data) {
-            var object = {value: data, timestamp: new Date().getTime()};
-            storage.setItem(uri, JSON.stringify(object));
-            callback(JSON.parse(storage.getItem(uri)).value);
+            storage.setItem(uri, data);
+            callback(storage.getItem(uri));
         });
     }
 
