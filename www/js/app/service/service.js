256e17f4bcda304793327f666520c0921c74630e
Uva <Uva@Uva-PC>
2016-12-26 23:05:48 -0300

    Va a buscar al server siempre si puede

--- www/js/app/service/service.js
+++ www/js/app/service/service.js
@@ -2,6 +2,7 @@ preguntio.service = (function () {
 
     var storage = window.localStorage;
     var tiempoCache = 604800000; //10 días
+    //var tiempoCache = 100; //no cache
 
     function get(uri, callback) {
         if (localStorage.getItem(uri) === null) {
@@ -14,12 +15,15 @@ preguntio.service = (function () {
             if (diferencia >= tiempoCache) {
                 getFromServer(uri, callback);
             } else {
+                console.log("cache");
                 callback(object.value);
+                getFromServer(uri,function(){});
             }
         }
     }
 
     function getFromServer(uri, callback) {
+        console.log("server");
         $$.get(uri, null, function (data) {
             var object = {value: data, timestamp: new Date().getTime()};
             storage.setItem(uri, JSON.stringify(object));
