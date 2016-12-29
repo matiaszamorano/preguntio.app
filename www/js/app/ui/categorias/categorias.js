63543b08d9f9867b4c7acaed401eb2a4f975d0d9
Uva <Uva@Uva-PC>
2016-12-29 00:09:08 -0300

    Arreglos en colecciones mostrando mas info

--- www/js/app/ui/categorias/categorias.js
+++ www/js/app/ui/categorias/categorias.js
@@ -5,7 +5,8 @@ preguntio.ui.categorias = (function () {
             var coleccion = JSON.parse(data);
             $$(".titulo-coleccion").text(coleccion.titulo);
             $$(".descripcion-coleccion").text(coleccion.descripcion);
-            $$(".imagen-categoria").css("background-image","url("+coleccion.imagen+")");
+            $$(".imagen-categoria").css("background-image", "url(" + coleccion.imagen + ")");
+            $$(".fuente").text(coleccion.fuente);
             preguntio.service.get(coleccion._links.categorias.href, function (data) {
                 var template = $$('#categoria-template').html();
                 var compiledTemplate = Template7.compile(template);
--- www/js/app/ui/colecciones/colecciones.js
+++ www/js/app/ui/colecciones/colecciones.js
@@ -1,12 +1,19 @@
 preguntio.ui.colecciones = (function () {
 
+    //var url = 'http://preguntio.herokuapp.com/';
+    var url = 'http://localhost:8080/'
+
     function init() {
         var template = $$('#coleccion-template').html();
 
         var compiledTemplate = Template7.compile(template);
 
-        preguntio.service.get('http://preguntio.herokuapp.com/api/colecciones?sort=id,desc', function (data) {
+        preguntio.service.get(url + 'api/colecciones?sort=id,desc', function (data) {
             var colecciones = JSON.parse(data)._embedded;
+            colecciones.colecciones.forEach(function (item) {
+                var tags = item.tags.split(",");
+                item.tags = tags;
+            });
             var html = compiledTemplate(colecciones);
             $$('#colecciones').html(html);
         });
